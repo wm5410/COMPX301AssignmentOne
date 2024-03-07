a5867f564f6eb9c85e0b0eb0291c0ee853b0c0fe
jp378 <jpoutoa865@gmail.com>
2024-03-07 20:12:49 +1300

    I am testing my commits - Justin

--- TestDoc.java
+++ TestDoc.java
@@ -2,5 +2,6 @@ public class TestDoc {
     //Hello World test
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("I got mine working - Justin");
     }
 }
