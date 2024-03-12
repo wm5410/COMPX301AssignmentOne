e07dc4ec626713a0c37eeb1bdde5c9c68e0e0fb9
William <William@DESKTOP-UAAQGI8>
2024-03-12 14:21:05 +1300

    Correct Files

--- TestDoc.java
+++
@@ -1,7 +0,0 @@
-public class TestDoc {
-    //Hello World test
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("I got mine working - Justin");
-    }
-}
