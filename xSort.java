b80c06ba35e99c4f70fbaec03c72b47e496b1f79
William <William@DESKTOP-UAAQGI8>
2024-03-18 13:43:27 +1300

    Small changes

--- xSort.java
+++ xSort.java
@@ -1,4 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+//need Priority queue 
+
 public class xSort {
-    //Need to do stuff
-    
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.err.println("Usage: java xSort <lines> <initial_runs_file> <merged runs>");
+            System.exit(1);
+        }
+
+        int m = Integer.parseInt(args[0]); // Number of lines in each initial run
+        String initialRunsFile = args[1]; // File with initial runs
+        int k = Integer.parseInt(args[2]); // Number of runs merged on each pass
+
+        try {
+            mergeRuns(initialRunsFile, m, k);
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static void mergeRuns(String initialRunsFile, int m, int k){
+        //test
+        System.out.println(initialRunsFile + " " + m + " " + k);
+    }
 }
